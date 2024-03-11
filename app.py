9de74bd8b731df82a9368702125199d67ac869ac
KristjanOmar <117899282+KristjanOmar@users.noreply.github.com>
2024-03-11 11:51:01 +0000

    Hotfix - Halló, heimur!

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def nafn():
     return "<h1>Verkefni 1</h1> <br> nafnið þitt"
+
+@app.route('/hallo_heimur')
+def hallo_heimur():
+    return "Halló, heimur!"
